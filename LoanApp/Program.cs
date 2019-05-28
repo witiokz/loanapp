fe3cb37e3c95ca1d331b6767551dc45c8f990a05
viktoras <viktoras@studiov4.com>
2019-05-28 11:30:21 +0300

    Init

---
+++ LoanApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using LoanApp.Services;
+using System;
+
+namespace LoanApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var loanService = new LoanService();
+
+            double.TryParse(args[0], out double loanAmount);
+            double.TryParse(args[1], out double interestRate);
+            int.TryParse(args[2], out int loanTermMonths);
+
+            var monthlyPay = loanService.CalculateMonthlyPayment(loanAmount, interestRate, loanTermMonths);
+
+            for (int i = 0; i < loanTermMonths; i++)
+            {
+                Console.WriteLine("Date: {0}", DateTime.Now.AddMonths(i + 1));
+                Console.WriteLine("Monthly Payment: {0} €", monthlyPay);
+                Console.WriteLine();
+            }
+
+            var arp = loanService.CalculateAnnualPercentageRates(loanAmount, (double)monthlyPay, loanTermMonths);
+
+            Console.WriteLine("APR: {0}", arp);
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ LoanApp/Services/LoanService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LoanApp.Services
+{
+    class LoanService
+    {
+        /// <summary>
+        public decimal CalculateMonthlyPayment(double loanAmount, double interestRate, double loanTermMonths)
+        {
+            var rate = interestRate / 12 / 100;
+
+            var denominator = Math.Pow((1 + rate), loanTermMonths) - 1;
+            var factor = rate + (rate / denominator);
+            var payment = (loanAmount * factor);
+
+            return (decimal)Math.Round(payment, 2);
+        }
+
+        public double CalculateAnnualPercentageRates(double loanAmount, double paymentAmount, double numberOfPayments)
+        {
+            double deposit = 0;
+            double p = loanAmount;
+            double a = paymentAmount;
+            double n = numberOfPayments;
+            double r = 1.01;
+            double pp = (p - deposit);
+            double rnew = 0;
+            double q = 0;
+
+            while (Math.Abs(r - rnew) > 0.0000001)
+            {
+                double rn = Math.Pow(r, n);
+                double rnm1 = rn / r;
+                double fac = (pp * rn - q) * (1 - r) / (1 - rn) - a;
+                double facdash = (n * pp * rnm1 * (1 - r) - (pp * rn - q)) / (1 - rn) + (pp * rn - q) * (1 - r) * n * rnm1 / ((1 - rn) * (1 - rn));
+                rnew = r - fac / facdash;
+                r = rnew;
+            }
+            double r12 = Math.Pow(r, 12);
+            double apr = 0.01 * Math.Round((r12 - 1) * 100 * 100);
+            return apr;
+        }
+    }
+}
+
+
